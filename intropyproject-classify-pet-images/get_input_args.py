d4cef2d0e4b5a9971452bad9f02a2464202715bc
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-07-04 18:03:19 +0200

    Create 3 command line arguments to parse

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -40,5 +40,11 @@ def get_input_args():
     """
     # Create Parse using ArgumentParser
     parser = argparse.ArgumentParser("Parse Image Folder, CNN Model Architecture and Text File")
-    
+    # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
+    parser.add_argument('--dir', type = str, default = 'pet_images/', 
+                    help = 'path to the folder of pet images')
+    parser.add_argument('--arch',type = str, default = 'vgg',
+                        help = 'Determine which CNN model architecture')
+    parser.add_argument('--dogfile',type = str, default = 'dognames.txt',
+                       help = 'Choose the file of dogs names')
     return None
